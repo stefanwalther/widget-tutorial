68669dd4a3881e6d6b2d945dc9b50b7f84cfdd0b
Stefan Walther <Stefan.Walther@qlik.com>
2016-06-15 03:16:33 +0200

    tutorial(prerequisites): download widget-exercise

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,5 +4,9 @@ import gulp from 'gulp';
 import zip from 'gulp-zip';
 
 
-
+gulp.task('default', () => {
+	return gulp.src('widget-exercises/*')
+		.pipe(zip('widget-exercises.zip'))
+		.pipe(gulp.dest('./src/tutorial/01-prerequisites/downloads'));
+});
 
